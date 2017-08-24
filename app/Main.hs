1d33501e6d41d63e3f6246a4a723306ef3403460
clockvoid <hidenari_1@yahoo.co.jp>
2017-08-24 11:49:32 +0900

    add: background behavior

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import View
+import Graphics.Gloss
 
 main :: IO ()
-main = print "Game of Life"
+main = display (InWindow "Game of Life" (round fieldWidth + 50, round fieldWidth + 50) (10, 10)) white $ pictures [backGround, cell (0, 0) black]
--- src/View.hs
+++ src/View.hs
@@ -1,13 +1,22 @@
 module View (
     cell
+    , backGround
+    , fieldWidth
+    , troutNumber
 ) where
 
 import Graphics.Gloss
 
 type Field = [[Color]]
 
+fieldWidth :: Float
+fieldWidth = 200
+
+troutNumber :: Float
+troutNumber = 20
+
 triangles :: Float -> Float -> (Path, Path)
-triangles x y = ([(x, y), (x + 5, y), (x, y - 5)], [(x + 5, y), (x, y - 5), (x + 5, y - 5)])
+triangles x y = ([(x, y), (x + fieldWidth / troutNumber, y), (x, y - fieldWidth / troutNumber)], [(x + fieldWidth / troutNumber, y), (x, y - fieldWidth / troutNumber), (x + fieldWidth / troutNumber, y - fieldWidth / troutNumber)])
 
 picture :: (Path, Path) -> Picture
 picture (a, b) = pictures [polygon a, polygon b]
@@ -19,4 +28,18 @@ cell (x, y) c = color c (picture (triangles x y))
 --coordinate field = [(x, y, c) | 
 
 --render :: Field -> Picture
+
+makeVerticalLines :: Float -> [Picture]
+makeVerticalLines x = if x <= fieldWidth / 2
+    then [line [(x, fieldWidth / 2), (x, -1 * fieldWidth / 2)]] ++ makeVerticalLines (x + fieldWidth / troutNumber)
+    else []
+
+makeHorizontalLines :: Float -> [Picture]
+makeHorizontalLines y = if y <= fieldWidth / 2
+    then [line [(fieldWidth / 2, y), (-1 * fieldWidth / 2, y)]] ++ makeHorizontalLines (y + fieldWidth / troutNumber)
+    else []
+
+
+backGround :: Picture
+backGround = pictures $ makeVerticalLines (-1 * fieldWidth / 2) ++ makeHorizontalLines (-1 * fieldWidth / 2)
 
