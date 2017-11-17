88c11ddec7e5045b4800e0bebcdbc7a00374f4d8
clockvoid <hidenari_1@yahoo.co.jp>
2017-11-17 20:19:47 +0900

    fix: interactive completion

--- src/View.hs
+++ src/View.hs
@@ -10,7 +10,7 @@ import Data.Maybe (maybe)
 
 type Field = [[Color]]
 
-data State = State (Maybe Path) Field
+data State = State (Maybe [(Int, Int)]) [[Bool]]
 
 cellSize :: Float
 cellSize = 10
@@ -18,53 +18,60 @@ cellSize = 10
 bgColor :: Color
 bgColor = white
 
-fieldToState :: Field -> State
+fieldToState :: [[Bool]] -> State
 fieldToState field = State Nothing field
 
 windowName :: String
 windowName = "Cell Automaton"
 
-nextField :: (Field -> Field) -> State -> State
-nextField next state
+nextField :: ([[Bool]] -> [[Bool]]) -> Float -> State -> State
+nextField next num state
   | State Nothing ss <- state
+  , _ <- num
   , _ <- next
   = fieldToState $ next ss
 
   | otherwise
   = state
 
-run :: Int -> Int -> [[a]] -> ([[a]] -> [[a]]) -> (a -> Color) -> IO ()
-run size fps initialState next stateToColor = play window bgColor fps (State Nothing initialState) renderState $ const . const (nextField next)
+run :: Int -> Int -> [[Bool]] -> ([[Bool]] -> [[Bool]]) -> (Bool -> Color) -> IO ()
+run size fps initialState next stateToColor = play window bgColor fps (State Nothing initialState) renderState handleEvent (nextField next)
   where
     s = (fromInteger . toInteger) size
     window = createWindow s windowName
-    renderState = (render s) . (map . map) stateToColor . pathToField
+    renderState = (render s) . (map . map) stateToColor. pathToField
 
-boolToInt :: Bool -> Int
-boolToInt b = if b then 1 else 0
+boolToInt :: Bool -> Color
+boolToInt b = if b then red else white
 
 cutArray :: [a] -> [[a]] -> [[a]]
 cutArray [] xs = xs
-cutArray ys xs = cutArray (drop cellSize ys) (take cellSize ys):xs
+cutArray ys xs = cutArray (drop 60 ys) ((take 60 ys):xs)
 
-pathToField :: State -> Field
+pathToField :: State -> [[Bool]]
 pathToField state
   | State (Just ps) ss <- state
-  = cutArray $ map boolToInt $ map (flip elem ps) [(x, y) | x <- [0..cellSize], y <- [0..cellSize]]
+  = cutArray (map (flip elem ps) [(x, y) | x <- [0..(60 - 1)], y <- [0..(60 - 1)]]) []
 
-floatToInt :: Float -> Int
-floatToInt num = fromIntegral $ floor (num + 400) / (800 / cellSize)
+  | State Nothing ss <- state
+  = ss
+
+convertX :: Float -> Int
+convertX num = round $ ((60 * cellSize) / 2 - num) / cellSize
+
+convertY :: Float -> Int
+convertY num = round $ ((60 * cellSize) / 2 - num) / cellSize
 
 handleEvent :: Event -> State -> State
 handleEvent event state
   | EventMotion (x, y) <- event
   , State (Just ps) ss <- state
-  = State (Just ((floatToInt x, floatToInt y):ps)) ss
+  = State (Just ((convertX x, convertY y):ps)) ss
 
   -- Start drawing a new line.
   | EventKey (MouseButton LeftButton) Down _ pt@(x,y) <- event
   , State Nothing ss       <- state
-  = State (Just [(floatToInt x, floatToInt y)]) []
+  = State (Just [(convertX x, convertY y)]) []
   
   -- Finish drawing a line, and add it to the picture.
   | EventKey (MouseButton LeftButton) Up _ pt@(x,y)      <- event
@@ -86,11 +93,8 @@ cell s (x, y, c) = color c (pictures (map polygon (triangles s x y)))
 coordinate :: Float -> Field -> [(Float, Float, Color)]
 coordinate s field = concat $ map (\(x, cs) -> [(x, fst y, snd y) | y <- zip [0..s - 1] cs]) $ zip [0..s - 1] field
 
-render :: Float -> State -> Picture
-render s state
-  | _ <- s
-  , State _ field <- state
-  = pictures (map (cell s) (coordinate s field) ++ [backGround s])
+render :: Float -> Field -> Picture
+render s field = pictures (map (cell s) (coordinate s field) ++ [backGround s])
 
 makeVerticalLines :: Float -> Float -> [Picture]
 makeVerticalLines s x = if x <= (cellSize * s) / 2
